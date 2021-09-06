7b39deb1b3a3141c9dba9b395df96650fafffd1d
max-pc <makskms@gmail.com>
2021-09-06 21:43:19 +1000

    lab3

--- AMD_Lab3/app/src/main/java/com/example/amd_lab3/MainActivity.java
+++ AMD_Lab3/app/src/main/java/com/example/amd_lab3/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import com.example.amd_lab3.R;
@@ -23,21 +24,28 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         TextView headerView = (TextView) findViewById(R.id.selectedMenuItem);
-        switch(id){
-            case R.id.action_settings :
-                headerView.setText("Настройки");
-                return true;
-            case R.id.open_settings:
-                headerView.setText("Открыть");
-                return true;
-            case R.id.save_settings:
-                headerView.setText("Сохранить");
-                return true;
-        }
+        setTitle(headerView, item);
+        item.setChecked(!item.isChecked());
+
         return super.onOptionsItemSelected(item);
     }
+
+    CharSequence getItemState(MenuItem item){
+        return item.isChecked() == true ? "выбран" : "не выбран";
+    }
+    void setTitle(TextView headerView, MenuItem item){
+        if (item.isCheckable()) {
+            headerView.setText("заголовок меню: " + item.getTitle() + " состояние флажка: " + getItemState(item));
+        } else {
+            headerView.setText("заголовок меню: " + item.getTitle());
+        }
+
+
+    }
+
 }
