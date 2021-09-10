cad7f862ab0bd537882b402492d513accbd42cd1
labtop_max <makskms1@gmail.com>
2021-09-10 15:39:44 +1000

    lab 5 stable
    переход между активностями с передачей данных вперед

--- Lab5/app/src/main/java/com/example/lab5/SecondActivity.java
+++ Lab5/app/src/main/java/com/example/lab5/SecondActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -12,6 +13,14 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        Bundle arguments = getIntent().getExtras();
+
+        User user;
+        if(arguments!=null) {
+            user = (User) arguments.getSerializable(User.class.getSimpleName());
+            String userInformation = "Имя: " + user.getName() + " пароль: " + user.getPassword();
+            Toast.makeText(this, userInformation, Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void showDialog(View v) {
