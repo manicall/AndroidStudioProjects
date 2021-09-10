17564a2f89e40a2874dadc96cba72cf763881be9
labtop_max <makskms1@gmail.com>
2021-09-10 14:26:41 +1000

    lab 5 stable

---
+++ Lab5/app/src/main/java/com/example/lab5/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.lab5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showDialog(View v) {
+        CustomDialogFragment dialog = new CustomDialogFragment();
+        dialog.show(getSupportFragmentManager(), "first");
+    }
+}
