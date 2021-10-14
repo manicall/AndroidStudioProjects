d8af7c2baf33d9b55ca88b9ee1f8756c52247eff
labtop_max <makskms1@gmail.com>
2021-10-15 09:23:15 +1000

    lab12 in progress

---
+++ lab12/app/src/main/java/com/example/lab12/SnowFlakeService.java
@@ -0,0 +1,93 @@
+package com.example.lab12;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+public class SnowFlakeService extends Service {
+    private WindowManager windowManager;
+    private ImageView floatingFaceBubble;
+
+
+    public void onCreate() {
+        super.onCreate();
+        floatingFaceBubble = new ImageView(this);
+        //a face floating bubble as imageView
+        floatingFaceBubble.setImageResource(R.drawable.ic_snowflake);
+        windowManager = (WindowManager)getSystemService(WINDOW_SERVICE);
+        //here is all the science of params
+        final LayoutParams myParams = new WindowManager.LayoutParams(
+                100,
+                100,
+                500,
+                500,
+                LayoutParams.TYPE_PHONE,
+                LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+        myParams.gravity = Gravity.TOP | Gravity.LEFT;
+
+        // add a floatingfacebubble icon in window
+        windowManager.addView(floatingFaceBubble, myParams);
+
+
+
+        //Animation animation = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        //image.startAnimation(animation);
+
+
+        try{
+            //for moving the picture on touch and slide
+            floatingFaceBubble.setOnTouchListener(new View.OnTouchListener() {
+                WindowManager.LayoutParams paramsT = myParams;
+                private int initialX;
+                private int initialY;
+                private float initialTouchX;
+                private float initialTouchY;
+                private long touchStartTime = 0;
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    //remove face bubble on long press
+                    if(System.currentTimeMillis()-touchStartTime>ViewConfiguration.getLongPressTimeout() && initialTouchX== event.getX()){
+                        windowManager.removeView(floatingFaceBubble);
+                        stopSelf();
+                        return false;
+                    }
+                    switch(event.getAction()){
+                        case MotionEvent.ACTION_DOWN:
+                            touchStartTime = System.currentTimeMillis();
+                            initialX = myParams.x;
+                            initialY = myParams.y;
+                            initialTouchX = event.getRawX();
+                            initialTouchY = event.getRawY();
+                            break;
+                        case MotionEvent.ACTION_UP:
+                            break;
+                        case MotionEvent.ACTION_MOVE:
+                            myParams.x = initialX + (int) (event.getRawX() - initialTouchX);
+                            myParams.y = initialY + (int) (event.getRawY() - initialTouchY);
+                            windowManager.updateViewLayout(v, myParams);
+                            break;
+                    }
+                    return false;
+                }
+            });
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
